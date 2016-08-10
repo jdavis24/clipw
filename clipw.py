2d04d9b0b89ddc4d5b21b921a0752e360256dc2f
rock <github@nocureforugly.com>
2016-08-10 14:29:02 -0400

    added switch to add special character to the end

--- clipw.py
+++ clipw.py
@@ -3,23 +3,33 @@
 from os import urandom
 from base64 import b64encode
 import argparse
+import random
+import string
 
 def default():
 	gen = urandom(12)
 	genout = b64encode(gen).decode('utf-8')
-	print(genout)
+	pw = genout.translate({ord(x): '' for x in '!@#$%^&*()_+-==[]{}<>,./\?`~'})
+	print(pw)
 def length(len):
 	gen = urandom(len)
 	genout = b64encode(gen).decode('utf-8')
-	print(genout)
+	pw = genout.translate({ord(x): '' for x in '!@#$%^&*()_+-=[]{}<>,./\?`~'})
+	print(pw)
+def special():
+	gen = urandom(12)
+	genout = b64encode(gen).decode('utf-8')
+	pw = genout.translate({ord(x): '' for x in '!@#$%^&*()_+-=[]{}<>,./\?`~'})
+	randchar = random.choice(string.punctuation)
+	print(pw + randchar)
 
 #adding ability to use arguments
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-d', action='store_true', help='default behavior - prints 12-character password') #try default=True again
+parser.add_argument('-d', action='store_true', help='default behavior - prints 12-byte password', default = True) #try default=True again
 parser.add_argument('-a', action='store_true', help='secondary test switch')
-#parser.add_argument('-l', action='store', dest='len', type=int, help='set the length of password provided')
-parser.add_argument('--len', '-l', action='store', dest='len', type=int, help='set the length of password provided')
+parser.add_argument('--spec', '-s', action='store_true', help='adds a special character to the default password')
+parser.add_argument('--len', '-l', action='store', dest='len', type=int, help='set the length of password provided in bytes')
 
 args = parser.parse_args()
 
@@ -27,12 +37,10 @@ if args.d:
 	default()
 elif args.a:
 	print('a switch worked')
-#elif args.l:
-#	print(args.len)
-#	length(args.len)
 elif args.len:
-#	print(args.len)
 	length(args.len)
+elif args.spec:
+	special()
 
 #default random output behavior
 #test = urandom(12)
